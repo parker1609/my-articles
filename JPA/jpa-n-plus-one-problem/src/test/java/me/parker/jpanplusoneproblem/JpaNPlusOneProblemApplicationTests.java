305bca5eabdccf13416f315337d154d218773e9e
parker1609 <psbum1609@gmail.com>
2022-05-22 23:21:20 +0900

    [JPA N+1 실습] init project

---
+++ JPA/jpa-n-plus-one-problem/src/test/java/me/parker/jpanplusoneproblem/JpaNPlusOneProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.parker.jpanplusoneproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNPlusOneProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
